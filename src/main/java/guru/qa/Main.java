0069152980d3d8e589778cd6773997951b463cdd
Kate Moiseenko <katfedoseeva@gmail.com>
2023-04-01 22:26:59 +0300

    4 rows for song added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     private static String songText = "Hello World\n" +
             "How've you been\n" +
-            "Good to see you, my old friend";
+            "Good to see you, my old friend\n" +
+            "Sometimes I feel, cold as steel\n" +
+            "Broken like I'm never gonna heal\n" +
+            "I see a light, little hole\n" +
+            "In the little world";
     public static void main(String[] args) {
         System.out.println(songText);
     }
