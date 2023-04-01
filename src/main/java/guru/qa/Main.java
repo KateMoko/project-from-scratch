48e41fc633bb1614bb4796234741027a09e82ed0
Ekaterina <katfedoseeva@gmail.com>
2023-04-01 22:31:23 +0300

    Song has been change to the correct one

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,9 +2,8 @@ package guru.qa;
 
 public class Main {
 
-    private static String songText = "Hello World\n" +
-            "How've you been\n" +
-            "Good to see you, my old friend";
+    private static String songText = "Hello, hello, hello world\n" +
+            "I open my eyes and say hello to the world";
     public static void main(String[] args) {
         System.out.println(songText);
     }
