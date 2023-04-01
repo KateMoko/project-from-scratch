48b13499d598cb3dbb53cbd053945c2ec66c5ba8
Kate Moiseenko <katfedoseeva@gmail.com>
2023-04-01 22:15:53 +0300

    Song text has been added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,11 @@
 package guru.qa;
 
 public class Main {
+
+    private static String songText = "Hello World\n" +
+            "How've you been\n" +
+            "Good to see you, my old friend";
     public static void main(String[] args) {
-        System.out.println("Hello from my 3rd HW!");
+        System.out.println(songText);
     }
 }
